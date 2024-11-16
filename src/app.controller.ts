e33aefe870bc9402a8d60fc91ef0bab40948c2f7
rhafaelc <rhafaelchandra@gmail.com>
2024-11-16 10:47:47 +0700

    refactor: remove unused enpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,33 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { Auth } from './decorators/auth.decorator';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  @Get('/morning')
-  @Auth('DOCTOR')
-  goodMorning() {
-    return 'Good Morning!';
-  }
-
-  @Get('/afternoon')
-  @Auth('PATIENT')
-  goodAfternoon() {
-    return 'Good Afternoon!';
-  }
-
-  @Get('/evening')
-  @Auth('PATIENT', 'DOCTOR')
-  goodEvening() {
-    return 'Good Evening!';
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   sayHello() {
-    console.log(typeof process.env.DB_HOST);
-    console.log(typeof process.env.DB_PORT);
-    console.log(typeof process.env.DB_USERNAME);
-    console.log(typeof process.env.DB_PASSWORD);
-    console.log(typeof process.env.DB_NAME);
-    return 'Hello!';
+    return this.appService.getHello();
   }
 }
