b258f36ddd01183cdde8a09d60d05dea19ee9f47
Rhafael Chandra <109317539+rhafaelc@users.noreply.github.com>
2024-12-01 18:47:57 +0700

    feat: add auth guard

--- src/genai/genai.controller.ts
+++ src/genai/genai.controller.ts
@@ -1,16 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { GenaiService } from './genai.service';
 import { ChatbotChatDto } from './dto/chatbot-chat.dto';
+import { Auth } from '../common/decorators/auth.decorator';
 
 @Controller('genai')
 export class GenaiController {
   constructor(private readonly genaiService: GenaiService) {}
+
   @Post('chatbot')
+  @Auth('DOCTOR', 'PATIENT')
   async chatbot(@Body() chatbotChatDto: ChatbotChatDto) {
     return this.genaiService.chat(chatbotChatDto.message);
   }
 
   @Post('rephrase')
+  @Auth('PATIENT')
   async rephrase(@Body() chatbotChatDto: ChatbotChatDto) {
     return this.genaiService.rephrase(chatbotChatDto.message);
   }
--- src/genai/genai.module.ts
+++ src/genai/genai.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { GenaiService } from './genai.service';
 import { GenaiController } from './genai.controller';
 import { VertexaiModule } from 'src/infrastructure/vertexai/vertexai.module';
+import { FirebaseAdmin } from 'src/infrastructure/firebase/firebase.setup';
 
 @Module({
   imports: [VertexaiModule],
   controllers: [GenaiController],
-  providers: [GenaiService],
+  providers: [GenaiService, FirebaseAdmin],
 })
 export class GenaiModule {}
