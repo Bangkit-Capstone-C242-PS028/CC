7a4c06791915166b7a195e72953f301cd291acfa
Rhafael Chandra <109317539+rhafaelc@users.noreply.github.com>
2024-12-07 06:21:29 +0700

    fix(forums): update char limit

--- src/forums/dto/create-forum-reply.dto.ts
+++ src/forums/dto/create-forum-reply.dto.ts
@@ -3,7 +3,6 @@ import { IsNotEmpty, IsString, MinLength, MaxLength } from 'class-validator';
 export class CreateForumReplyDto {
   @IsString()
   @IsNotEmpty()
-  @MinLength(10)
   @MaxLength(500)
   content: string;
 }
